8a3d48dedcb82782d6910ff3023f042f629e0d32
Leyla Firatli <leylafiratli@leylas-mbp.home>
2020-03-10 14:43:12 -0700

    first commit

---
+++ client/build/precache-manifest.5e732327fb6df86d86bc722fbc4fcd99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827efc16f28f9bea4024ce967f9a231f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "267f3f271b005373383b",
+    "url": "/static/css/2.34ca2c66.chunk.css"
+  },
+  {
+    "revision": "e60749fc0d92f4d8196d",
+    "url": "/static/css/main.24fd82e8.chunk.css"
+  },
+  {
+    "revision": "267f3f271b005373383b",
+    "url": "/static/js/2.2b0a89b5.chunk.js"
+  },
+  {
+    "revision": "e60749fc0d92f4d8196d",
+    "url": "/static/js/main.9337ff70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
